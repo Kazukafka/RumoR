8b02ffc18d661bbfdaeba3e152e6629dcf307ede
Kazukafka <pasuwaado135@gmail.com>
2021-11-16 20:34:23 +0900

    cognito lambda trigger

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import config from './src/aws-exports';
 
@@ -16,6 +16,8 @@ Amplify.configure(config);
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  Auth.currentAuthenticatedUser().then(console.log);
 
   if (!isLoadingComplete) {
     return null;
